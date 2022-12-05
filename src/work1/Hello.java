904b662d91e6054bdfd8b5e81a9b327a5cee9151
FRANCISKA TOTH <totu.mihaela@yahoo.com>
2022-12-04 16:18:25 -0800

    Added more work

--- src/work1/Hello.java
+++ src/work1/Hello.java
@@ -3,5 +3,6 @@ package work1;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Work1 started");
+        System.out.println("Created Git Repository");
     }
 }
